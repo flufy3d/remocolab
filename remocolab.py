13db6f1730074484a3c86c07ef2169a2ac7e1e41
flufy3d <flufy3d@gmail.com>
2022-06-03 18:55:27 +0200

    back to xfce

--- remocolab.py
+++ remocolab.py
@@ -366,11 +366,7 @@ def _setupVNC():
   my_apt.installDebPackage("virtualgl.deb")
   my_apt.installDebPackage("turbovnc.deb")
 
-  my_apt.installPkg("gnome-session")
-  my_apt.installPkg("gdm3")
-  my_apt.installPkg("tasksel")
-  my_apt.installPkg("ubuntu-desktop")
-  
+  my_apt.installPkg("xfce4", "xfce4-terminal")
   my_apt.commit()
   my_apt.close()
 
